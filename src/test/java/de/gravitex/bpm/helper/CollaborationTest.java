95efd8450e3dd0b0d129eb5ab9844bbab526c4bc
Sts <Sts@view-localhost>
2020-03-28 08:45:38 +0100

    123

--- src/test/java/de/gravitex/bpm/helper/CollaborationTest.java
+++ src/test/java/de/gravitex/bpm/helper/CollaborationTest.java
@@ -9,6 +9,7 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import de.gravitex.bpm.helper.base.BpmEngineTest;
+import de.gravitex.bpm.helper.constant.ProcessConstants;
 import de.gravitex.bpm.helper.runner.CollaborationRunner;
 
 public class CollaborationTest extends BpmEngineTest {
@@ -19,23 +20,28 @@ public class CollaborationTest extends BpmEngineTest {
 	@Test
 	@Deployment(resources = { "collaborationTest.bpmn" })
 	public void testM2_S1_S4_M5_S5() {
-		
+
 		CollaborationRunner runner1 = CollaborationRunner.withProcessEngine(processEngine);
 		CollaborationRunner runner2 = CollaborationRunner.withProcessEngine(processEngine);
 
 		ProcessInstance masterProcessInstance1 = runner1.toM2();
 		ProcessInstance masterProcessInstance2 = runner2.toM2();
-		
+
 		ProcessInstance slaveProcessInstance1 = runner1.toS1(masterProcessInstance1);
 		ProcessInstance slaveProcessInstance2 = runner2.toS1(masterProcessInstance2);
-		
+
 		runner1.toM5(masterProcessInstance1, slaveProcessInstance1);
 		runner2.toM5(masterProcessInstance2, slaveProcessInstance2);
-		
+
 		runner1.toS5(masterProcessInstance1, slaveProcessInstance1);
 		runner2.toS5(masterProcessInstance2, slaveProcessInstance2);
+
+		// all mains gone...
+		assertEquals(0, processEngine.getRuntimeService().createProcessInstanceQuery()
+				.processDefinitionId(ProcessConstants.Main.DEF.DEF_MAIN_PROCESS).list().size());
 		
-		// all 'another slaves' must be gone, too...
-		assertEquals(0, processEngine.getRuntimeService().createProcessInstanceQuery().list().size());
+		// all slaves gone...
+		assertEquals(0, processEngine.getRuntimeService().createProcessInstanceQuery()
+				.processDefinitionId(ProcessConstants.Slave.DEF.DEF_SLAVE_PROCESS).list().size());
 	}
 }
